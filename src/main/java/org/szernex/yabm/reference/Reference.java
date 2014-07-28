611c4b3413bf95a98e9248dde2c1372d1db0e831
Szernex <szernex@gmail.com>
2014-07-28 12:42:21 +0200

    * refactored build script; versions are now stored in build.properties so build.gradle doesn't have to be touched anymore

--- src/main/java/org/szernex/yabm/reference/Reference.java
+++ src/main/java/org/szernex/yabm/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MOD_ID = "YABM";
 	public static final String MOD_NAME = "Yet Another Backup Mod";
-	public static final String VERSION = "1.7.10-0.1";
+	public static final String VERSION = "@VERSION@";
 
 	public static final String CLIENT_PROXY_CLASS = "org.szernex.yabm.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "org.szernex.yabm.proxy.ServerProxy";
