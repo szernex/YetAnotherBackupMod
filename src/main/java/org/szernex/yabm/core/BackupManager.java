b9ec99f5d3603d5e0076b9518451bfcf471eb836
Szernex <szernex@gmail.com>
2014-08-13 16:59:04 +0200

    + added prototype FTP upload functionality + added additional library commons-net for FTP capabilities
    * refactored thread handling into separate class BackupManager
    * fixed world auto save not being turned off properly
    * fixed running backup task in actual Thread

---
+++ src/main/java/org/szernex/yabm/core/BackupManager.java
@@ -0,0 +1,157 @@
+package org.szernex.yabm.core;
+
+import org.szernex.yabm.handler.ConfigHandler;
+import org.szernex.yabm.util.ChatHelper;
+import org.szernex.yabm.util.FileHelper;
+import org.szernex.yabm.util.LogHelper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class BackupManager implements Runnable
+{
+	private BackupTask backupTask = new BackupTask();
+	private FTPTask ftpTask = new FTPTask();
+
+
+	private boolean isPersistentBackup()
+	{
+		if (!ConfigHandler.persistentEnabled)
+		{
+			return false;
+		}
+
+		String archive_name = FileHelper.getArchiveFileName(ConfigHandler.backupPrefix, true);
+
+		archive_name = archive_name.substring(0, archive_name.lastIndexOf("_"));
+
+		try
+		{
+			File target_path = new File(ConfigHandler.persistentLocation).getCanonicalFile();
+
+			if (!target_path.exists())
+			{
+				if (!target_path.mkdirs())
+				{
+					LogHelper.error("Could not create persistent backup directory %s", target_path.toString());
+					return false;
+				}
+
+				return true;
+			}
+
+			File[] files = target_path.listFiles(new FileHelper.BackupFileFilter(archive_name));
+
+			return (files.length == 0);
+		}
+		catch (IOException ex)
+		{
+			LogHelper.error("Error during persistent backup detection: %s" , ex.getMessage());
+			ex.printStackTrace();
+			return false;
+		}
+	}
+
+	private void consolidateBackups()
+	{
+		if (ConfigHandler.maxBackupCount <= 0)
+		{
+			return;
+		}
+
+		String archive_name = FileHelper.getArchiveFileName(ConfigHandler.backupPrefix, false);
+
+		try
+		{
+			File target_path = new File(ConfigHandler.backupLocation).getCanonicalFile();
+
+			if (!target_path.exists())
+			{
+				return;
+			}
+
+			File[] files = target_path.listFiles(new FileHelper.BackupFileFilter(archive_name));
+
+			if (files.length <= ConfigHandler.maxBackupCount)
+			{
+				return;
+			}
+
+			Arrays.sort(files);
+
+			files = Arrays.copyOfRange(files, 0, files.length - ConfigHandler.maxBackupCount);
+			ChatHelper.sendServerChatMsg(ChatHelper.getLocalizedMsg("yabm.backup.general.backup_consolidation", files.length));
+
+			for (File f : files)
+			{
+				if (f.delete())
+				{
+					LogHelper.info("Deleted old backup %s", f);
+				}
+				else
+				{
+					LogHelper.warn("Could not delete old backup %s", f);
+				}
+			}
+		}
+		catch (IOException ex)
+		{
+			LogHelper.error("Error during consolidation: %s", ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+
+	private void startAndWaitForThread(Runnable task)
+	{
+		Thread thread = new Thread(task);
+
+		thread.start();
+
+		try
+		{
+			thread.join();
+		}
+		catch (InterruptedException ex)
+		{
+			LogHelper.error("Thread got interrupted: %s", ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run()
+	{
+		backupTask.init((isPersistentBackup() && !ConfigHandler.ftpEnabled ? ConfigHandler.persistentLocation : ConfigHandler.backupLocation),
+		                ConfigHandler.backupPrefix,
+		                ConfigHandler.backupList,
+		                ConfigHandler.compressionLevel
+		);
+
+		startAndWaitForThread(backupTask);
+
+		if (ConfigHandler.ftpEnabled)
+		{
+			if (backupTask.getLastBackupFile() != null)
+			{
+				ftpTask.init(backupTask.getLastBackupFile(),
+				             ConfigHandler.ftpServer,
+				             ConfigHandler.ftpPort,
+				             ConfigHandler.ftpUsername,
+				             ConfigHandler.ftpPassword,
+				             ConfigHandler.ftpLocation
+				);
+			}
+
+			startAndWaitForThread(ftpTask);
+		}
+
+
+		consolidateBackups();
+	}
+
+	public void startBackup()
+	{
+		new Thread(this).start();
+	}
+}
--- src/main/java/org/szernex/yabm/handler/BackupTickHandler.java
+++ src/main/java/org/szernex/yabm/handler/BackupTickHandler.java
@@ -2,17 +2,17 @@ package org.szernex.yabm.handler;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
+import org.szernex.yabm.YABM;
 import org.szernex.yabm.core.BackupTask;
-import org.szernex.yabm.util.*;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
+import org.szernex.yabm.core.FTPTask;
+import org.szernex.yabm.util.ScheduleHelper;
+import org.szernex.yabm.util.Time;
 
 public class BackupTickHandler
 {
 	Time nextSchedule;
 	BackupTask backupTask = new BackupTask();
+	FTPTask ftpTask = new FTPTask();
 
 	public BackupTickHandler()
 	{
@@ -31,114 +31,12 @@ public class BackupTickHandler
 			return;
 		}
 
-		startBackup();
+		YABM.backupManager.startBackup();
 		updateScheduleTime();
-	}
-
-	public void startBackup()
-	{
-		if (isPersistentBackup())
-		{
-			backupTask.init(ConfigHandler.persistentLocation, ConfigHandler.backupPrefix, ConfigHandler.backupList, ConfigHandler.compressionLevel);
-		}
-		else
-		{
-			backupTask.init(ConfigHandler.backupLocation, ConfigHandler.backupPrefix, ConfigHandler.backupList, ConfigHandler.compressionLevel);
-		}
-
-		new Thread(backupTask).run();
-		consolidateBackups();
 	}
 
 	public void updateScheduleTime()
 	{
 		nextSchedule = ScheduleHelper.getNextSchedule(ConfigHandler.backupSchedule.trim().split(" "));
-	}
-
-	private boolean isPersistentBackup()
-	{
-		if (!ConfigHandler.persistentEnabled)
-		{
-			return false;
-		}
-
-		String archive_name = FileHelper.getArchiveFileName(ConfigHandler.backupPrefix, true);
-
-		archive_name = archive_name.substring(0, archive_name.lastIndexOf("_"));
-
-		try
-		{
-			File target_path = new File(ConfigHandler.persistentLocation).getCanonicalFile();
-
-			if (!target_path.exists())
-			{
-				if (!target_path.mkdirs())
-				{
-					LogHelper.error("Could not create persistent backup directory %s", target_path.toString());
-					return false;
-				}
-
-				return true;
-			}
-
-			File[] files = target_path.listFiles(new FileHelper.BackupFileFilter(archive_name));
-
-			return (files.length == 0);
-		}
-		catch (IOException ex)
-		{
-			LogHelper.error("Error during persistent backup detection: %s" , ex.getMessage());
-			ex.printStackTrace();
-			return false;
-		}
-	}
-
-	private void consolidateBackups()
-	{
-		if (ConfigHandler.maxBackupCount <= 0)
-		{
-			return;
-		}
-
-		String archive_name = FileHelper.getArchiveFileName(ConfigHandler.backupPrefix, false);
-
-		try
-		{
-			File target_path = new File(ConfigHandler.backupLocation).getCanonicalFile();
-
-			if (!target_path.exists())
-			{
-				return;
-			}
-
-			File[] files = target_path.listFiles(new FileHelper.BackupFileFilter(archive_name));
-
-			if (files.length <= ConfigHandler.maxBackupCount)
-			{
-				return;
-			}
-
-			Arrays.sort(files);
-
-			files = Arrays.copyOfRange(files, 0, files.length - ConfigHandler.maxBackupCount);
-			ChatHelper.sendServerChatMsg(ChatHelper.getLocalizedMsg("yabm.backup.general.backup_consolidation", files.length));
-
-			for (File f : files)
-			{
-				if (f.delete())
-				{
-					LogHelper.info("Deleted old backup %s", f);
-				}
-				else
-				{
-					LogHelper.warn("Could not delete old backup %s", f);
-				}
-			}
-		}
-		catch (IOException ex)
-		{
-			LogHelper.error("Error during consolidation: %s", ex.getMessage());
-			ex.printStackTrace();
-		}
 	}
 }
